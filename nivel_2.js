974e857b477672f40373bf9e9cda869b2258707e
Mabiel-rd <mabielrosario12@gmail.com>
2022-01-12 12:20:24 -0400

    nivel2

---
+++ nivel_2.js
@@ -0,0 +1,5 @@
+//Sin embargo, esto es sólo un ejemplo inicial y no tiene demasiado sentido. Vamos con algo más útil... Si nos fijamos en su primer carácter ¿Cómo podríamos seleccionar sólo el personaje feliz
+
+<input pattern="
+84921132A
+"></input>
